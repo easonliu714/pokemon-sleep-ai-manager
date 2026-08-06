3a8d743bc033e6af42a968d95a431021a3f0c9bd
Liu Chia Yi <eason714@gmail.com>
2026-08-06 09:17:06 +0800

    fix(v0.3.90): restore service-worker stale-cache audit contract

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,15 @@ const ASSETS = [
 ];
 
 self.addEventListener('install',(event)=>{event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',(event)=>{event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE).map((key)=>caches.delete(key)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window',includeUncontrolled:true})).then((clients)=>Promise.all(clients.map((client)=>client.postMessage({type:'pokemon-sleep-version-activated',app_version:APP_VERSION,build:APP_BUILD})))));});
+self.addEventListener('activate',(event)=>{
+  event.waitUntil(
+    caches.keys()
+      .then((keys)=>Promise.all(keys.filter((key) => key !== CACHE).map((key)=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+      .then(()=>self.clients.matchAll({type:'window',includeUncontrolled:true}))
+      .then((clients)=>Promise.all(clients.map((client)=>client.postMessage({type:'pokemon-sleep-version-activated',app_version:APP_VERSION,build:APP_BUILD}))))
+  );
+});
 self.addEventListener('fetch',(event)=>{
   if(event.request.method!=='GET')return;
   const url=new URL(event.request.url);const sameOrigin=url.origin===self.location.origin;
